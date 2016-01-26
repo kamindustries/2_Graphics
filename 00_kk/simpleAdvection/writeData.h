909921ef49f0f4fdc416ce25088a5165df9c075a
Kurt Kaminski <kurtoon@gmail.com>
2016-01-25 23:36:39 -0800

    updating advection writeData

--- 00_kk/simpleAdvection/writeData.h
+++ 00_kk/simpleAdvection/writeData.h
@@ -34,50 +34,8 @@ void write(const char* _filename, float4* _img, int _width, int _height, int _st
     free(data);
 }
 
-void writeImage(float4* _data, int _increment, int _width, int _height, int _stride) {
-  // float4* img_ptr = (float4*)malloc(sizeof(float4)*DIM*DIM);
-  // checkCudaErrors (cudaMemcpy(img_ptr, displayPtr, sizeof(float4)*DIM*DIM, cudaMemcpyDeviceToHost ));
-
-  // float *img_ptr = (float*)malloc(sizeof(float)*_width*_height*_stride);
-  // glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_FLOAT, img_ptr);
-
-  // if (_write_txt){
-  //   char filename_txt[1024 * sizeof(int) / 3 + 2];
-  //   sprintf(filename_txt, "data/cuda_x%d.txt", _increment);
-  //   write(filename_txt, img_ptr, DIM, DIM, 4);
-  // }
-  // if (_write_img){
-    char filename_png[2048 * sizeof(int) / 3 + 2];
-    sprintf(filename_png, "data/images/velocity/cuda_x%05d.png", _increment);
-    writePNG(filename_png, _data, _width, _height, _stride);
-  // }
-  // free(img_ptr);
+void writeImage(const char *_filename, float4* _data, int _increment, int _width, int _height, int _stride) {
+  char path[2048 * sizeof(int) / 3 + 2];
+  snprintf(path, sizeof(path), "%s%05d.png", _filename, _increment);
+  writePNG(path, _data, _width, _height, _stride);
 }
-
-// void writeImage(float4* _data, int _increment, int _width, int _height, int _stride) {
-//   int dim = _width * _height;
-//   int size = dim * _stride;
-//   float* data_f = new float[size];
-//
-//   for (int y=0; y<_height; y++){
-//     for (int x=0; x<_width; x++){
-//       int pos = (y*_height) + x;
-//       data_f[pos+0] = _data[pos].x;
-//       data_f[pos+1] = _data[pos].y;
-//       data_f[pos+2] = _data[pos].z;
-//       data_f[pos+3] = _data[pos].w;
-//     }
-//   }
-//   writeImage(data_f, _increment, _width, _height, _stride );
-//   delete[] data_f;
-//
-//   // for (int i=0; i < dim; i++){
-//     // data_f[i] = 0.0;
-//     // data_f[(i*_stride)+0] = _data[i].x;
-//     // data_f[(i*_stride)+1] = _data[i].y;
-//     // data_f[(i*_stride)+2] = _data[i].z;
-//     // data_f[(i*_stride)+3] = _data[i].w;
-//   // }
-//
-//
-// }
